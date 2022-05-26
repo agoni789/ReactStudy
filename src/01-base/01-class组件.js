3b550a6a99a22dbeff58d25987f994a66715e703
cm <303671950@qq.com>
2022-05-27 00:44:43 +0800

    初识模块化

---
+++ src/01-base/01-class组件.js
@@ -0,0 +1,30 @@
+// class Test{
+//     constructor() {
+//         this.a=1
+//     }
+//     testa(){
+//         console.log("testa")
+//     }
+//
+// }
+// class ChildTest extends Test{
+//     testb(){
+//
+//     }
+// }
+// var obj = new ChildTest()
+// console.log(obj.testa())
+
+import React from "react";
+class App extends React.Component{
+    render() {
+        return <div>hello react
+        <ul>
+            <li>111</li>
+            <li>2222</li>
+        </ul>
+        </div>
+    }
+}
+
+export default App
--- src/index.js
+++ src/index.js
@@ -1,19 +1,23 @@
 // console.log("qwert")
 import React from "react";
 import ReactDom from 'react-dom'
+import App from './01-base/01-class组件'
+
+
+ReactDom.render(<App></App>,document.getElementById("root"))
 
 //jsx  js+xml
 
-ReactDom.render(<div>
-    <b>123123</b>
-    <ul>
-        <li>111</li>
-        <li>222</li>
-        <li>333</li>
-    </ul>
-</div>,document.getElementById("root"))
-
-ReactDom.render(React.createElement("div",{
-    id:"aaa",
-    class:"bbb"
-},"1111111111"),document.getElementById("root1"))
+// ReactDom.render(<div>
+//     <b>123123</b>
+//     <ul>
+//         <li>111</li>
+//         <li>222</li>
+//         <li>333</li>
+//     </ul>
+// </div>,document.getElementById("root"))
+//
+// ReactDom.render(React.createElement("div",{
+//     id:"aaa",
+//     className:"bbb"
+// },"1111111111"),document.getElementById("root1"))
