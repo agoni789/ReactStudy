48021478222a64c324649122c6ef868741bf5907
cm <303671950@qq.com>
2022-05-29 23:24:03 +0800

    状态改变另一种写法

--- src/01-base/08-state.js
+++ src/01-base/08-state.js
@@ -3,20 +3,29 @@ import React, {Component} from 'react';
 class App extends
 Component
 {
-    state = {
-        myText: "收藏",
-        myShow: true
+    // state = {
+    //     myText: "收藏",
+    //     myShow: true
+    // }
+    constructor() {
+        super();
+        this.state = {
+            myText: "收藏",
+            myShow: true,
+            myName:"CM"
+        }
     }
     render()
     {
         return (
             <div>
-                <h1>欢迎来到react开发</h1>
+                <h1>欢迎来到react开发-{this.state.myName}</h1>
                 <button onClick={()=>{
                     // this.state.myText = "取消收藏" //不能直接修改状态
                     this.setState({
                         // myText:"取消收藏"
-                        myShow:!this.state.myShow
+                        myShow:!this.state.myShow,
+                        myName:"xiaoming"
                     })//间接修改状态
                     if(this.state.myShow){
                         console.log("收藏的逻辑")
