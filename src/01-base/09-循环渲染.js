92b7e015020b0e70add049aa685913c7084f7008
cm <303671950@qq.com>
2022-05-30 12:40:54 +0800

    列表中key的使用

--- src/01-base/09-循环渲染.js
+++ src/01-base/09-循环渲染.js
@@ -3,18 +3,40 @@ import React, {Component} from 'react';
 class App extends Component
 {
     state={
-        list:["111","222","333"]
+        list:[
+            {
+                id:1,
+                text:"111"
+            },
+            {
+                id:2,
+                text:"222"
+            },
+            {
+                id:3,
+                text:"333"
+            },
+        ]
     }
     render()
     {
-        var newlist =  this.state.list.map(item => <li key={item}>{item}</li>)
+        var newlist =  this.state.list.map(item => <li key={item.id}>{item.text}</li>)
         return (
             <div>
-                { newlist}
+                {newlist}
+                {
+                    this.state.list.map((item,index) => <li key={index}>{item.text}</li>)
+                }
             </div>
         );
     }
 }
+/*
+为了列表的复用和重排,设置key,提高性能
+理想的key item.id
+
+不涉及列表增加 删除 重排  设置成索引也行
+ */
 
 export default App;
 /*
